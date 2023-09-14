303088fbe70b5e07cd29ae3c9f9c013109b3d6a4
melihovodina <melihoxa@gmail.com>
2023-09-14 18:38:07 +0300

    2 prblms solved

--- integer to roman/integer to roman/integer to roman/Program.cs
+++ integer to roman/integer to roman/integer to roman/Program.cs
@@ -1,77 +1,24 @@
-﻿public class Solution
+﻿using System.Text;
+
+public class Solution
 {
     public string IntToRoman(int num)
     {
-        string total = "";
-        int tries = num;
-        for (int i = 0; num != 0; i++)
+        Dictionary<int, string> nums = new Dictionary<int, string>() 
         {
-            if (num >= 1000)
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+        StringBuilder roman = new StringBuilder();
+        foreach (var item in nums)
+        {
+            while (num >= item.Key)
             {
-                num -= 1000;
-                total += "M";
-            }
-            else if (num >= 900)
-            {
-                num -= 900;
-                total += "CM";
-            }
-            else if (num >= 500)
-            {
-                num -= 500;
-                total += "D";
-            }
-            else if (num >= 400)
-            {
-                num -= 400;
-                total += "CD";
-            }
-            else if (num >= 100)
-            {
-                num -= 100;
-                total += "C";
-            }
-            else if (num >= 90)
-            {
-                num -= 90;
-                total += "XC";
-            }
-            else if (num >= 50)
-            {
-                num -= 50;
-                total += "L";
-            }
-            else if (num >= 40)
-            {
-                num -= 40;
-                total += "XL";
-            }
-            else if (num >= 10)
-            {
-                num -= 10;
-                total += "X";
-            }
-            else if (num >= 9)
-            {
-                num -= 9;
-                total += "IX";
-            }
-            else if (num >= 5)
-            {
-                num -= 5;
-                total += "V";
-            }
-            else if (num >= 4)
-            {
-                num -= 4;
-                total += "IV";
-            }
-            else if (num >= 1)
-            {
-                num -= 1;
-                total += "I";
+                    roman.Append(item.Value);
+                    num -= item.Key;
             }
         }
-        return total;
+        return roman.ToString();
     }
 }
