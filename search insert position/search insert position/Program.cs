53432070665ec3e250ab430731daa4e19790999c
melihovodina <melihoxa@gmail.com>
2023-10-13 15:56:47 +0300

    solved 1 problem

---
+++ search insert position/search insert position/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] nums = new int[4] { 1, 3, 5, 6 };
+int target = 5;
+Solution sol = new Solution();
+Console.WriteLine(sol.SearchInsert(nums, target));
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int index = Array.BinarySearch(nums, target);
+        return index >= 0 ? index : ~index;
+    }
+}
