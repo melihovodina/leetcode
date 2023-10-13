a7d4411e0a17afa5b654ae1d138ee43b7b6fbe4d
melihovodina <melihoxa@gmail.com>
2023-10-13 11:35:51 +0300

    solved 2 problems

---
+++ remove duplicates from sorted array/remove duplicates from sorted array/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] nums = new int[10] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+Solution sol = new Solution();
+Console.WriteLine(sol.RemoveDuplicates(nums));
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int temp = int.MinValue;
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (temp != nums[i])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+            temp = nums[i];
+        }
+        return k;
+    }
+}
---
+++ valid parentheses/valid parentheses code/valid parentheses code/Program.cs
@@ -0,0 +1,25 @@
+﻿Solution sol = new Solution();
+Console.WriteLine(sol.IsValid("{[]}"));
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0) return false;
+                char last = stack.Pop();
+                if (c == ')' && last != '(') return false;
+                if (c == '}' && last != '{') return false;
+                if (c == ']' && last != '[') return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
