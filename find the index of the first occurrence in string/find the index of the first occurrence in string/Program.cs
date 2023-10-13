05769eaa3c2ca4b0c8efd22e21e9a602820a9b8a
melihovodina <melihoxa@gmail.com>
2023-10-13 15:32:37 +0300

    solved 1 problem and update 1 solution

---
+++ find the index of the first occurrence in string/find the index of the first occurrence in string/Program.cs
@@ -0,0 +1,14 @@
+﻿string haystack = "sadbutsad";
+string needle = "g";
+Solution sol = new Solution();
+Console.WriteLine(sol.StrStr(haystack, needle));
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int k = 0;
+        //if (needle.All(c => haystack.Contains(c)))
+            k = haystack.IndexOf(needle);
+        return k;
+    }
+}
---
+++ two sum short/two sum short/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] nums = new int[5] { 2, 7, 11, 15, 12 };
+int target = 9;
+Solution sol = new Solution();
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] arr = new int[2];
+        for(int i = 0; i < nums.Length - 1; i++)
+        {
+            for(int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target && i != j)
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+    }
+}
